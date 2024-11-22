caa935bffe0c95421d325d38a160c630887b6e83
unknown <3122215002098esm@ITLAB.SSN>
2024-11-22 11:10:00 +0530

    Initial commit

---
+++ sample.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args)
+    {
+        String str = "selcia", nstr = "";
+        char ch;
+
+        for (int i = 0; i < str.length(); i++) {
+            ch = str.charAt(i);
+            nstr = ch + nstr; 
+        }
+        System.out.println(nstr);
+    }
+}
